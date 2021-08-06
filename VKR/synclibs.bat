66d0547c33048e7b028f51e80b60ba5363336fd9
DESKTOP-516INA8\Nic <nicvanzuylen@gmail.com>
2021-08-06 20:11:56 +1000

    Changed:
    -synclibs.bat now resets repositories to a specific commit using a SHA1.

--- VKR/synclibs.bat
+++ VKR/synclibs.bat
@@ -1,7 +1,10 @@
 mkdir External
 cd External
-git clone https://github.com/nothings/stb.git stb
-git clone https://github.com/tinyobjloader/tinyobjloader.git tinyobjloader
-git clone https://github.com/glfw/glfw.git glfw
-git clone https://github.com/g-truc/glm.git glm
+call ../clonerepo.bat https://github.com/nothings/stb.git stb 5a0bb8b
+call ../clonerepo.bat https://github.com/tinyobjloader/tinyobjloader.git tinyobjloader 51908fb
+call ../clonerepo.bat https://github.com/glfw/glfw.git glfw 201400b
+call ../clonerepo.bat https://github.com/g-truc/glm.git glm 06ed280
+
 cd ..\
+
+pause
